5c5b3fcc9d85d78f17cbe3ca9d2b7aaf2d12e323
Huzaif Ali <sharukhali3105@gmail.com>
2025-12-10 11:07:47 +0530

    Create menu.py

--- menu.py
+++ menu.py
@@ -1,3 +1,9 @@
+menu = [
+    [1, "Idly", 30],
+    [2, "Dosa", 40],
+    [3, "Fried Rice", 120],
+    [4, "Veg Burger", 80]
+]
 def show_menu():
     print("\n--- MENU ---")
     print("Code   Item            Price")
