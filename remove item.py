61b1bc0cb703e2da6f66ce258daee35b26266065
Huzaif Ali <sharukhali3105@gmail.com>
2025-12-10 11:15:29 +0530

    Delete remove item.py

--- remove item.py
+++
@@ -1,8 +0,0 @@
-def remove_item():
-    code = int(input("Enter item code to remove: "))
-    for item in menu:
-        if item[0] == code:
-            menu.remove(item)
-            print("Item removed.")
-            return
-    print("Invalid code.")
