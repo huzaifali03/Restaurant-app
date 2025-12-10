0e377f0ee3d147b7f5a17e9156c2c3a347dd0e00
Huzaif Ali <sharukhali3105@gmail.com>
2025-12-10 11:15:10 +0530

    Delete add item to menu.py

--- add item to menu.py
+++
@@ -1,6 +0,0 @@
-def add_item():
-    code = int(input("Enter item code: "))
-    name = input("Enter item name: ")
-    price = int(input("Enter price: "))
-    menu.append([code, name, price])
-    print("Item added successfully.")
